3757bcee0d67a7743c9d28a58981ff8b1a3f51af
Shivam Aggarwal <shivam01101995@gmail.com>
2020-10-13 16:08:43 +0530

    NullObjectPattern

---
+++ src/com/shivam/patterns/NullObjectPattern.java
@@ -0,0 +1,22 @@
+package com.shivam.patterns;
+
+/*
+ * Null Object Pattern
+ * 
+ * Null object pattern 
+ * 
+ * Problem with null is that if something is nullable, then it either has a value or doesn't have a value ie is null.
+ * This introduces additional branching in the code in the sense that if something has a value then it follows one path and if it
+ * doesn't have a value then it follows another path. It results in extra branching, extra checks needed for null.
+ * 
+ * Whenever we see a null, there's a high likelyhood that we could replace it with a Nullable Object.
+ * 
+ */
+public class NullObjectPattern {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
